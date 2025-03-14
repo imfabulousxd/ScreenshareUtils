25066d8d19aa7ab1b7d25d36c3a3fae784e62295
imfabulousxd <154655943+imfabulousxd@users.noreply.github.com>
2025-03-14 04:34:42 +0330

    Update SSUtils.ps1

--- SSUtils.ps1
+++ SSUtils.ps1
@@ -28,16 +28,165 @@ Start-Sleep -s 3
 
 Clear-Host
 
+$global:Is64BitOperatingSystem = [System.Environment]::Is64BitOperatingSystem
+
 function CreateLibsDir {
     if (-Not(Test-Path -Path libs)) {
         New-Item -Name "libs" -ItemType "directory"
     }
 }
 
+function Get-FileFromWeb { # Credits: https://gist.github.com/ChrisStro/37444dd012f79592080bd46223e27adc
+    param (
+        # Parameter help description
+        [Parameter(Mandatory)]
+        [string]$URL,
+  
+        # Parameter help description
+        [Parameter(Mandatory)]
+        [string]$File 
+    )
+    Begin {
+        function Show-Progress {
+            param (
+                # Enter total value
+                [Parameter(Mandatory)]
+                [Single]$TotalValue,
+        
+                # Enter current value
+                [Parameter(Mandatory)]
+                [Single]$CurrentValue,
+        
+                # Enter custom progresstext
+                [Parameter(Mandatory)]
+                [string]$ProgressText,
+        
+                # Enter value suffix
+                [Parameter()]
+                [string]$ValueSuffix,
+        
+                # Enter bar lengh suffix
+                [Parameter()]
+                [int]$BarSize = 40,
+
+                # show complete bar
+                [Parameter()]
+                [switch]$Complete
+            )
+            
+            # calc %
+            $percent = $CurrentValue / $TotalValue
+            $percentComplete = $percent * 100
+            if ($ValueSuffix) {
+                $ValueSuffix = " $ValueSuffix" # add space in front
+            }
+            if ($psISE) {
+                Write-Progress "$ProgressText $CurrentValue$ValueSuffix of $TotalValue$ValueSuffix" -id 0 -percentComplete $percentComplete            
+            }
+            else {
+                # build progressbar with string function
+                $curBarSize = $BarSize * $percent
+                $progbar = ""
+                $progbar = $progbar.PadRight($curBarSize,[char]9608)
+                $progbar = $progbar.PadRight($BarSize,[char]9617)
+        
+                if (!$Complete.IsPresent) {
+                    Write-Host -NoNewLine "`r$ProgressText $progbar [ $($CurrentValue.ToString("#.###").PadLeft($TotalValue.ToString("#.###").Length))$ValueSuffix / $($TotalValue.ToString("#.###"))$ValueSuffix ] $($percentComplete.ToString("##0.00").PadLeft(6)) % complete"
+                }
+                else {
+                    Write-Host -NoNewLine "`r$ProgressText $progbar [ $($TotalValue.ToString("#.###").PadLeft($TotalValue.ToString("#.###").Length))$ValueSuffix / $($TotalValue.ToString("#.###"))$ValueSuffix ] $($percentComplete.ToString("##0.00").PadLeft(6)) % complete"                    
+                }                
+            }   
+        }
+    }
+    Process {
+        try {
+            $storeEAP = $ErrorActionPreference
+            $ErrorActionPreference = 'Stop'
+        
+            # invoke request
+            $request = [System.Net.HttpWebRequest]::Create($URL)
+            $response = $request.GetResponse()
+  
+            if ($response.StatusCode -eq 401 -or $response.StatusCode -eq 403 -or $response.StatusCode -eq 404) {
+                throw "Remote file either doesn't exist, is unauthorized, or is forbidden for '$URL'."
+            }
+  
+            if($File -match '^\.\\') {
+                $File = Join-Path (Get-Location -PSProvider "FileSystem") ($File -Split '^\.')[1]
+            }
+            
+            if($File -and !(Split-Path $File)) {
+                $File = Join-Path (Get-Location -PSProvider "FileSystem") $File
+            }
+
+            if ($File) {
+                $fileDirectory = $([System.IO.Path]::GetDirectoryName($File))
+                if (!(Test-Path($fileDirectory))) {
+                    [System.IO.Directory]::CreateDirectory($fileDirectory) | Out-Null
+                }
+            }
+
+            [long]$fullSize = $response.ContentLength
+            $fullSizeMB = $fullSize / 1024 / 1024
+  
+            # define buffer
+            [byte[]]$buffer = new-object byte[] 1048576
+            [long]$total = [long]$count = 0
+  
+            # create reader / writer
+            $reader = $response.GetResponseStream()
+            $writer = new-object System.IO.FileStream $File, "Create"
+  
+            # start download
+            $finalBarCount = 0 #show final bar only one time
+            do {
+          
+                $count = $reader.Read($buffer, 0, $buffer.Length)
+          
+                $writer.Write($buffer, 0, $count)
+              
+                $total += $count
+                $totalMB = $total / 1024 / 1024
+          
+                if ($fullSize -gt 0) {
+                    Show-Progress -TotalValue $fullSizeMB -CurrentValue $totalMB -ProgressText "Downloading $($File.Name)" -ValueSuffix "MB"
+                }
+
+                if ($total -eq $fullSize -and $count -eq 0 -and $finalBarCount -eq 0) {
+                    Show-Progress -TotalValue $fullSizeMB -CurrentValue $totalMB -ProgressText "Downloading $($File.Name)" -ValueSuffix "MB" -Complete
+                    $finalBarCount++
+                    #Write-Host "$finalBarCount"
+                }
+
+            } while ($count -gt 0)
+        }
+  
+        catch {
+        
+            $ExeptionMsg = $_.Exception.Message
+            Write-Host "Download breaks with error : $ExeptionMsg"
+        }
+  
+        finally {
+            # cleanup
+            if ($reader) { $reader.Close() }
+            if ($writer) { $writer.Flush(); $writer.Close() }
+        
+            $ErrorActionPreference = $storeEAP
+            [GC]::Collect()
+        }    
+    }
+}
+
 function Everything {
     CreateLibsDir
     if (-Not(Test-Path -Path libs/Everything.exe)) {
-        Invoke-WebRequest "https://www.voidtools.com/Everything-1.4.1.1026.x64-Setup.exe" -OutFile libs/Everything.exe
+        if ($global:Is64BitOperatingSystem) {
+            Invoke-WebRequest "https://www.voidtools.com/Everything-1.4.1.1026.x64-Setup.exe" -OutFile libs/Everything.exe
+        } else {
+            Invoke-WebRequest "https://www.voidtools.com/Everything-1.4.1.1026.x86-Setup.exe" -OutFile libs/Everything.exe
+        }
     }
     .\libs\Everything.exe
 }
@@ -45,7 +194,11 @@ function Everything {
 function DownloadEverythingCLI {
     CreateLibsDir
     if (-Not(Test-Path -Path libs/ES)) {
-        Invoke-WebRequest "https://www.voidtools.com/ES-1.1.0.27.x64.zip" -OutFile libs/ES.zip
+        if ($global:Is64BitOperatingSystem) {
+            Invoke-WebRequest "https://www.voidtools.com/ES-1.1.0.27.x64.zip" -OutFile libs/ES.zip
+        } else {
+            Invoke-WebRequest "https://www.voidtools.com/ES-1.1.0.27.x86.zip" -OutFile libs/ES.zip
+        }
         Expand-Archive -DestinationPath "libs/ES" -Path "libs/ES.Zip"
         Remove-Item -Path "libs/ES.zip"
     }
@@ -55,29 +208,50 @@ $global:7zPath = ""
 function Find7Zip {
     DownloadEverythingCLI
     $global:7zPath = .\libs\ES\es.exe "Program Files\7-Zip\7z.exe"
-    if ($global:7zPath -eq "") {
-        throw [CustomException]::new("Couldn't find 7zip installed")
+    if ($global:7zPath.Length -eq 0) {
+        throw [System.IO.FileNotFoundException]::new("Couldn't find 7zip installed")
     }
     $global:7zPath = $global:7zPath.Split("`r`n")[0]
+}
+
+$global:githubToken = ""
+$global:githubTokenAsked = $false
+function Get-GithubToken {
+    if ($global:githubTokenAsked) {
+        Return
+    }
+    $global:githubToken = Read-Host "Please enter your github token"
+    $global:githubTokenAsked = $true
+    Clear-Host
+}
+
+function Get-GithubAPI-Headers {
+    if ($global:githubToken.Length -eq 0) {
+        return @{}
+    } else {
+        return @{"Authorization"= "Bearer $global:githubToken"}
+    }
 }
 
 function Download7Zip {
     try {
         Find7Zip
-    } catch [CustomException] {
+    } catch [System.IO.FileNotFoundException] {
         Write-Host "Installing 7zip..."
         if (-Not(Test-Path -Path libs/7z.exe)) {
             CreateLibsDir
+            Get-GithubToken
             $url = "https://api.github.com/repos/ip7z/7zip/releases/latest"
-            $resp = Invoke-WebRequest $url
+            $resp = Invoke-WebRequest $url -Headers $(Get-GithubAPI-Headers)
             if ($resp.StatusCode -ne 200) {
                 Write-Host "Status code $($resp.StatusCode)"
                 Pause
                 Return
             }
             $content = $resp.Content | ConvertFrom-Json
+            $regex = if ($global:Is64BitOperatingSystem) {"7z\d+-x64\.exe"} else {"7z\d+\.exe"}
             foreach ($asset in $content.assets) {
-                if ($asset.name.EndsWith("x64.exe")) {
+                if ($asset.name -imatch $regex) {
                     Invoke-WebRequest $asset.browser_download_url -OutFile libs/7z.exe
                     break
                 }
@@ -86,6 +260,7 @@ function Download7Zip {
         .\libs\7z.exe
         Write-Host "After installing, Press enter..."
         Pause
+        Clear-Host
         Find7Zip
     }
 }
@@ -224,7 +399,8 @@ function WinPrefetchView {
     CreateLibsDir
     Download7Zip
     if (-Not(Test-Path -Path libs/wpv)) {
-        Invoke-WebRequest "https://www.nirsoft.net/utils/winprefetchview-x64.zip" -OutFile libs/wpv.zip
+        $url = if ($global:Is64BitOperatingSystem) {"https://www.nirsoft.net/utils/winprefetchview-x64.zip"} else {"https://www.nirsoft.net/utils/winprefetchview.zip"}
+        Invoke-WebRequest $url -OutFile libs/wpv.zip
         & $global:7zPath e -olibs/wpv libs/wpv.zip > $null
         Remove-Item -Path "libs/wpv.zip"
     }
@@ -234,8 +410,9 @@ function WinPrefetchView {
 function SystemInformer {
     CreateLibsDir
     if (-Not(Test-Path -Path libs/systeminformer.exe)) {
+        Get-GithubToken
         $url = "https://api.github.com/repos/winsiderss/si-builds/releases/latest"
-        $resp = Invoke-WebRequest $url
+        $resp = Invoke-WebRequest -Uri $url -Headers $(Get-GithubAPI-Headers)
         if ($resp.StatusCode -ne 200) {
             Write-Host "Status code $($resp.StatusCode)"
             Pause
@@ -255,8 +432,9 @@ function SystemInformer {
 function BamParser {
     CreateLibsDir
     if (-Not(Test-Path -Path libs/BAMParser.exe)) {
+        Get-GithubToken
         $url = "https://api.github.com/repos/spokwn/BAM-parser/releases/latest"
-        $resp = Invoke-WebRequest $url
+        $resp = Invoke-WebRequest $url -Headers $(Get-GithubAPI-Headers)
         if ($resp.StatusCode -ne 200) {
             Write-Host "Status code $($resp.StatusCode)"
             Pause
@@ -276,8 +454,9 @@ function BamParser {
 function JournalTrace {
     CreateLibsDir
     if (-Not(Test-Path -Path libs/JournalTrace.exe)) {
+        Get-GithubToken
         $url = "https://api.github.com/repos/spokwn/JournalTrace/releases/latest"
-        $resp = Invoke-WebRequest $url
+        $resp = Invoke-WebRequest $url -Headers $(Get-GithubAPI-Headers)
         if ($resp.StatusCode -ne 200) {
             Write-Host "Status code $($resp.StatusCode)"
             Pause
@@ -297,8 +476,9 @@ function JournalTrace {
 function PathsParser {
     CreateLibsDir
     if (-Not(Test-Path -Path libs/PathsParser.exe)) {
+        Get-GithubToken
         $url = "https://api.github.com/repos/spokwn/PathsParser/releases/latest"
-        $resp = Invoke-WebRequest $url
+        $resp = Invoke-WebRequest $url -Headers $(Get-GithubAPI-Headers)
         if ($resp.StatusCode -ne 200) {
             Write-Host "Status code $($resp.StatusCode)"
             Pause
@@ -338,6 +518,72 @@ function Services {
     Pause
 }
 
+function Hayabusa {
+    CreateLibsDir
+    if (-Not(Test-Path -Path libs/Hayabusa/Hayabusa.exe)) {
+        Get-GithubToken
+        $url = "https://api.github.com/repos/Yamato-Security/hayabusa/releases/latest"
+        $resp = Invoke-WebRequest $url -Headers $(Get-GithubAPI-Headers)
+        if ($resp.StatusCode -ne 200) {
+            Write-Host "Status code $($resp.StatusCode)"
+            Pause
+            Return
+        }
+        $content = $resp.Content | ConvertFrom-Json
+        $fileName = ""
+        $regex = if ($global:Is64BitOperatingSystem) {".*win-x64\.zip"} else {".*win-x86\.zip"}
+        foreach ($asset in $content.assets) {
+            if ($asset.name -imatch $regex) {
+                $fileName =$asset.name
+                Invoke-WebRequest $asset.browser_download_url -OutFile libs/Hayabusa.zip
+                break
+            }
+        }
+        Find7Zip
+        & $global:7zPath x -olibs/Hayabusa libs/Hayabusa.zip > $null
+        Remove-Item -Path "libs/Hayabusa.zip"
+        Rename-Item -Path "libs/Hayabusa/$($fileName.Remove($fileName.Length-4)).exe" -NewName "Hayabusa.exe"
+    }
+    .\libs\Hayabusa\Hayabusa.exe csv-timeline -l -o hayabusa.csv -U
+}
+
+function FTKImager {
+    CreateLibsDir
+    if (-Not(Test-Path -Path libs/FTKImager.exe)) {
+        $url = "https://d1kpmuwb7gvu1i.cloudfront.net/AccessData_FTK_Imager_4.7.1.exe"
+        Invoke-WebRequest $url -OutFile libs/FTKImager.exe
+    }
+    & .\libs\FTKImager.exe
+}
+
+function USBDriveLog {
+    CreateLibsDir
+    if (-Not(Test-Path -Path libs/USBDriveLog.exe)) {
+        Download7Zip
+        $url = "https://www.nirsoft.net/utils/usbdrivelog.zip"
+        Invoke-WebRequest $url -OutFile libs/USBDriveLog.zip
+        & $global:7zPath x -olibs/USBDriveLog libs/USBDriveLog.zip
+        Remove-Item -Path "libs/USBDriveLog.zip"
+    }
+    & .\libs\USBDriveLog\USBDriveLog.exe
+}
+
+function DetectItEasy {
+    CreateLibsDir
+    if (-Not(Test-Path -Path libs/DetectItEasy/die.exe)) {
+        Download7Zip
+        $url = if ($global:Is64BitOperatingSystem) {"https://github.com/horsicq/DIE-engine/releases/download/3.10/die_win64_portable_3.10_x64.zip"} else {"https://github.com/horsicq/DIE-engine/releases/download/3.10/die_win32_portable_3.10_x86.zip"}
+        Invoke-WebRequest $url -OutFile libs/DetectItEasy.zip
+        & $global:7zPath x -olibs/DetectItEasy libs/DetectItEasy.zip
+        Remove-Item -Path "libs/DetectItEasy.zip"
+    }
+    & .\libs\DetectItEasy\die.exe
+}
+
+function Cleanup {
+    Remove-Item -Path libs -Recurse
+}
+
 while (1) {
     Clear-Host
     Write-Host @"
@@ -349,6 +595,11 @@ while (1) {
 - jrnl: spokwn's JournalTrace
 - pathsparser: spokwn's PathsParser
 - services: see services and their uptime
+- hybs: Hayabusa
+- ftkimg: FTKImager
+- usbdl: USBDriveLog
+- die: Detect-It-Easy
+- cleanup: to cleanup after screenshare
 - exit: to exit
 "@
     $userInput = Read-Host "Enter one of the options above"
@@ -357,6 +608,8 @@ while (1) {
     try {
         if ($userInput.Equals("exit")) {
             break
+        } elseif ($userInput.Equals("cleanup")) {
+            Cleanup
         } elseif ($userInput.Equals("everything")) {
             Everything
         } elseif ($userInput.Equals("alt")) {
@@ -373,6 +626,14 @@ while (1) {
             PathsParser
         } elseif ($userInput.Equals("services")) {
             Services
+        } elseif ($userInput.Equals("hybs")) {
+            Hayabusa
+        } elseif ($userInput.Equals("ftkimg")) {
+            FTKImager
+        } elseif ($userInput.Equals("usbdl")) {
+            USBDriveLog
+        } elseif ($userInput.Equals("die")) {
+            DetectItEasy
         } else {
             Write-Host "Invalid option..."
             Start-Sleep -s 1
